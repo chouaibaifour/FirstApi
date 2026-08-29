02333bdb2fc717ddaf546c9772463f6a53fe7cbd
chouaibaifour <aifourchouaib03@gmail.com>
2026-08-29 15:18:53 +0100

    Enforce Middleware pipeline with Exception Handling

--- FristApi.Api/Program.cs
+++ FristApi.Api/Program.cs
@@ -14,16 +14,24 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseHttpLogging();
-    app.UseDeveloperExceptionPage();
+   app.UseExceptionHandler("/error-local-development");
 }
+else
+    app.UseExceptionHandler("/error");
+
 
 
 
 app.UseStaticFiles();
-app.UseWelcomePage();
 app.UseRouting();
+app.UseWelcomePage();
+
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/error", () => "An error occurred in production");
+app.MapGet("/error-local-development", () => "An error occurred in development");
+
 app.MapGet("/person", () => new Person("Andrew", "Lock")); 
+
 app.Run();
 public record Person(string FirstName, string LastName);
